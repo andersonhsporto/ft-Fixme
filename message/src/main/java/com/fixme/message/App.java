66443f00bd6ab33abee1d46d574c16edc25c679d
Anderson Porto <anderson.higo2@gmail.com>
2024-04-15 00:16:32 -0300

    Initial commit

---
+++ message/src/main/java/com/fixme/message/App.java
@@ -0,0 +1,13 @@
+package com.fixme.message;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
