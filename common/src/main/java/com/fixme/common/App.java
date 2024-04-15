495f6b6453aabaea483a200b6f867d75b3c410a3
Anderson Porto <anderson.higo2@gmail.com>
2024-04-15 18:15:29 -0300

    Refactor: shared code

--- common/src/main/java/com/fixme/common/App.java
+++ common/src/main/java/com/fixme/common/App.java
@@ -1,4 +1,4 @@
-package com.fixme;
+package com.fixme.common;
 
 /**
  * Hello world!
--- common/src/test/java/com/fixme/common/AppTest.java
+++ common/src/test/java/com/fixme/common/AppTest.java
@@ -1,4 +1,4 @@
-package com.fixme;
+package com.fixme.common;
 
 import junit.framework.Test;
 import junit.framework.TestCase;
